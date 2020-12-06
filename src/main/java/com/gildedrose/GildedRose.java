a01c4149f8593bdc276f272cca477fb1eecca349
elaltas <elaltas@gmail.com>
2020-12-06 13:20:38 +0100

    added all possible tests

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,6 @@
 package com.gildedrose;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
 class GildedRose {
     Item[] items;
@@ -12,66 +10,119 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        Stream.of(items).forEach(item -> item = updateItem(item));
+        Arrays.stream(items).forEach(this::age);
     }
 
-    public Item updateItem(Item item) {
-        boolean itemIsSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
-        boolean itemIsAgesBrie = item.name.equals("Aged Brie");
-        boolean itemIsConjuredManaCake = item.name.equals("Conjured Mana Cake");
-        boolean itemIsBackstagePasses = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-
-        if (isRegisteredItem(item)) {
-            if (itemIsSulfuras) {
-
-            } else if (itemIsAgesBrie) {
-                item.sellIn--;
-                item.quality++;
-
-            } else if(itemIsBackstagePasses) {
-                if( item.sellIn > 5 && item.sellIn <=10 ){
-                    item.sellIn--;
-                    item.quality = item.quality + 2;
-                }else if(item.sellIn >= 0 && item.sellIn <= 5){
-                    item.sellIn--;
-                    item.quality = item.quality + 3;
-                }else{
-                    item.sellIn--;
-                    item.quality++;
+    private void age(Item item) {
+        boolean aged_brie = item.name.equals("Aged Brie");
+        if (!aged_brie
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
                 }
-            } else {
-                if (itemIsConjuredManaCake) {
-                    item.quality--;
-                }
-                decreaseItem(item);
             }
         } else {
-            item.name = "fixme";
-            decreaseItem(item);
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
         }
-        item = itemIsSulfuras? item: sellInAndQualityProperties(item);
-        return item;
-    }
 
-
-    private void decreaseItem(Item item) {
-        item.sellIn--;
-        item.quality--;
-    }
-
-    private boolean isRegisteredItem(Item item) {
-        List<String> registeredItem = Arrays.asList("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
-        return registeredItem.contains(item.name);
-    }
-
-    private Item sellInAndQualityProperties(Item item) {
-        if (item.quality > 50) {
-            item.quality = 50;
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
         }
+
         if (item.sellIn < 0) {
-            item.sellIn = 0;
+            if (!aged_brie) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
         }
-        return item;
     }
+
+//    //New code
+//    public Item updateItem2(Item item) {
+//        boolean itemIsSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
+//        boolean itemIsAgesBrie = item.name.equals("Aged Brie");
+//        boolean itemIsConjuredManaCake = item.name.equals("Conjured Mana Cake");
+//        boolean itemIsBackstagePasses = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+//
+//        if (isRegisteredItem(item)) {
+//            if (itemIsSulfuras) {
+//
+//            } else if (itemIsAgesBrie) {
+//                item.sellIn--;
+//                item.quality++;
+//
+//            } else if(itemIsBackstagePasses) {
+//                if( item.sellIn > 5 && item.sellIn <=10 ){
+//                    item.sellIn--;
+//                    item.quality = item.quality + 2;
+//                }else if(item.sellIn >= 0 && item.sellIn <= 5){
+//                    item.sellIn--;
+//                    item.quality = item.quality + 3;
+//                }else{
+//                    item.sellIn--;
+//                    item.quality++;
+//                }
+//            } else {
+//                if (itemIsConjuredManaCake) {
+//                    item.quality--;
+//                }
+//                standardItemDecrease(item);
+//            }
+//        } else {
+//            item.name = "fixme";
+//            standardItemDecrease(item);
+//        }
+//        item = itemIsSulfuras? item: sellInAndQualityProperties(item);
+//        return item;
+//    }
+//
+//
+//    private void standardItemDecrease(Item item) {
+//        item.sellIn--;
+//        item.quality--;
+//    }
+//
+//    private boolean isRegisteredItem(Item item) {
+//        List<String> registeredItem = Arrays.asList("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
+//        return registeredItem.contains(item.name);
+//    }
+//
+//    private Item sellInAndQualityProperties(Item item) {
+//        if (item.sellIn < 0) {
+//            item.sellIn = 0;
+//        }
+//        if (item.quality > 50) {
+//            item.quality = 50;
+//        return item;
+//    }
+//*/
 
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] {new ItemBuilder().setName("foo").setSellIn(0).setQuality(0).createItem()};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
